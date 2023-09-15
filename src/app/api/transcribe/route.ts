73ff7cbb0da9e83395297b22eeed94677585a3a6
jegss22 <111718715+jegss22@users.noreply.github.com>
2023-09-15 18:19:18 +0800

    add: cloudinary as file storage
    1. save to cloudinary
    2. convert the secure url to a file formdata

--- src/app/api/transcribe/route.ts
+++ src/app/api/transcribe/route.ts
@@ -3,6 +3,7 @@ import { table, minifyData } from "../../utils/airtable"
 import { convertTextFormat } from "../../utils/format"
 import { OpenAIClient, AzureKeyCredential } from '@azure/openai';
 import axios from 'axios';
+import { cloudinary } from "../../utils/cloudinary"
 
 export async function GET() {
     try {
@@ -80,8 +81,84 @@ export async function PUT(req: Request) {
       }
 }
 
+/**
+ * cloudinary
+ */
 
-  
+export async function POST(req: Request){
+  try {
+   
+    const { id, url } = await req.json()
+    let video_url = url;
+
+    const videoUrl = video_url
+    const whisperApiKey = process.env.WHISPER_API_KEY;
+
+    const result = await cloudinary.uploader
+    .upload(videoUrl,{
+      resource_type: "video"
+    })
+
+    const readable = await createFormData(result.secure_url,'tmp.mp4')
+    const fileName = readable.get('tmp.mp4')
+
+    const formData = new FormData();
+    formData.append('file', fileName);
+    formData.append('model', 'whisper-1');
+    formData.append('response_format', 'srt');
+
+    const transcribeResponse = await axios.post(
+      'https://api.openai.com/v1/audio/transcriptions',
+      formData,
+      {
+        headers: {
+          Authorization: `Bearer ${whisperApiKey}`,
+        },
+      }
+    );
+
+    const formattedData = convertTextFormat(transcribeResponse.data)
+    
+    // update to jp_fix column
+    const updatedField = {
+      'jp_text': formattedData
+    }
+    const transcribeText = await table.update(id, updatedField)
+
+    return new NextResponse(JSON.stringify(transcribeText), {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    })
+
+
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ "message": "Missing required data" })
+  }
+}
+
+
+export const createFormData = async (url: string, fieldName: string): Promise<FormData | null> => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch the file from ${url}`);
+    }
+
+    const blob = await response.blob();
+    const formData = new FormData();
+    formData.append(fieldName, blob, 'tmp.mp4');
+
+    return formData;
+  } catch (error) {
+    console.error('Error creating FormData:', error);
+    return null;
+  }
+}
 
 
 
